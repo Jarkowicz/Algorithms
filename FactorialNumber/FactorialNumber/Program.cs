29411cbbf01bd353fe34a8f754ba7d3752e2e63e
Jarosław Grabowicz <jarek.grabowicz@outlook.com>
2022-01-11 19:23:59 +0100

    factorial number

---
+++ FactorialNumber/FactorialNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter number to generate factorial number:");
+bool parsed = int.TryParse(Console.ReadLine(), out int input);
+if(parsed)
+{
+    Console.WriteLine("factorial number: {0}! = {1}", input, GenerateFactorialNumber(input));
+}
+Console.WriteLine();
+
+int GenerateFactorialNumber(int input)
+{
+    int result = 1;
+    for (int i = input; i > 1; i--)
+    {
+        result *= i;
+    }
+
+    return result;
+}

fac2e66c287b8f14e683c31bbadc1c9a327b40b7
Jarosław Grabowicz <jarek.grabowicz@outlook.com>
2022-01-11 18:06:42 +0100

    First two algorithms Fibonacci and check prime number

---
+++ CheckPrimeNumber/CheckPrimeNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Check is it Prime number");
+Console.WriteLine("To exit press C");
+bool parsed = true;
+
+while (parsed)
+{    
+    Console.WriteLine("Enter input number:");
+
+    parsed = int.TryParse(Console.ReadLine(), out int result);
+    if (parsed)
+    {
+        var defaultColor = Console.ForegroundColor;
+        if(CheckIsPrimeNumber(result, out string output))
+            Console.ForegroundColor = ConsoleColor.Green;
+        else
+            Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(output);
+        Console.ForegroundColor = defaultColor;
+    }
+}
+
+bool CheckIsPrimeNumber(int input, out string output)
+{
+    output = "Prime number";
+    for (int i = 1; i <= input; i++)
+    {
+        if (i != 1 && i != input && input % i == 0)
+        {
+            output = "Not prime number";
+            return false;
+        }
+    }
+    return true;
+}
---
+++ FibonacciSeries/FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter number to generte fib numbers: ");
+int input = int.Parse(Console.ReadLine());
+Console.WriteLine(GenerateFibonacciIteration(input));
+
+
+string GenerateFibonacciIteration(int input)
+{
+    var n0 = 0;
+    var n1 = 1;
+    string series = $"{n0} {n1}";
+    for (int i = 2; i < input; i++)
+    {
+        var n2 = n0 + n1;
+        series += $" {n2}";
+        n0 = n1;
+        n1 = n2;
+    }
+
+    return series;
+}

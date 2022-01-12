b3e44d206bd2e22e570e498198e2600783e521be
Jarosław Grabowicz <jarek.grabowicz@outlook.com>
2022-01-12 14:45:42 +0100

    Added method to reverse numbers

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Reverse number");
+Console.WriteLine("Enter the number:");
+var parsed = int.TryParse(Console.ReadLine(), out int input);
+
+if (!parsed)
+    Console.WriteLine("Cannot parse the number!");
+else
+    Console.WriteLine(ReverseNumber(input));
+
+int ReverseNumber(int input)
+{
+    int reversedNumber = 0;
+    while (input > 0)
+    {
+        var digit = input % 10;        
+        reversedNumber = reversedNumber * 10 + digit;        
+        input /= 10;
+    }
+    return reversedNumber;
+}

0fda590a076eca574f254234847ef44198cd6904
Jarosław Grabowicz <jarek.grabowicz@outlook.com>
2022-01-12 14:26:39 +0100

    Added method to calculate sum of the digits

---
+++ SumOfTheDigit/SumOfTheDigit/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Calculate sum of the digits");
+Console.WriteLine("Enter the number");
+var parsed = int.TryParse(Console.ReadLine(), out int input);
+
+Console.WriteLine($"The sum of the digits of the number {input} = {SumOfTheDigits(input)}");
+
+int SumOfTheDigits(int input)
+{
+    var sum = 0;
+    while(input > 0)
+    {
+        int digit = input % 10;
+        sum += digit;
+        input /= 10;
+    }
+
+    return sum;
+}
